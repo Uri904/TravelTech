d99ad8fe34121b9d8ef62aea2405338ceaf8a4b7
Daniel Cumplido García <cumplidodaniel34@gmail.com>
2025-03-03 18:43:38 -0600

    Nuevo archivo para Crear Gastos y Quitado de la barra de MainPage.xmls y Crear.xmls de detalles

---
+++ TravelTech/Views/Gastos/CrearGasto.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelTech.Views.Gastos
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CrearGasto : ContentPage
+	{
+		public CrearGasto ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
