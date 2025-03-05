9e29a65c24691607166aa81d2711d3194985502b
Brian <Brian@DESKTOP-897DV2I>
2025-03-04 20:04:28 -0600

    Interfaz Agregar_Actividad

---
+++ TravelTech/Views/ActividadesDestinos/Agregar_Actividad.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelTech.Views.ActividadesDestinos
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Agregar_Actividad : ContentView
+	{
+		public Agregar_Actividad ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
