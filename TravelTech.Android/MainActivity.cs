f5c1f3d39b9261bd3841d55e9e2c76250b2b1b3b
Daniel Cumplido García <cumplidodaniel34@gmail.com>
2025-03-15 13:42:04 -0600

    icono

--- TravelTech.Android/MainActivity.cs
+++ TravelTech.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace TravelTech.Droid
 {
-    [Activity(Label = "TravelTech", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "TravelTech", Icon = "@mipmap/icontravel", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
